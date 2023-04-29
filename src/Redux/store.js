a2f5abed1d53cdc6a0c0ca1d2fbc3224dbd02f70
Toema04 <s.toema04@gmail.com>
2023-04-29 16:36:19 +0300

    Changing pachking from react app to vite

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
-import {rootReducer} from "./RootReducer"
-import { composeWithDevTools } from 'redux-devtools-extension';
-import {thunk} from "redux-thunk"
-
+import { rootReducer } from "./RootReducer";
 
 export const store = createStore(
-    rootReducer, /* preloadedState, */
- +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+	rootReducer /* preloadedState, */,
+	+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
